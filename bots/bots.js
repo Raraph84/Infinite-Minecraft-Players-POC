2b3729f143a5ec49acc8b6948d8e8025eb861c49
Raraph84 <contact@raraph.fr>
2024-07-23 18:32:18 +0200

    Do not require NodeJS for running the POC

--- bots/bots.js
+++ bots/bots.js
@@ -10,7 +10,7 @@ const spawn = (i) => {
 
     const bot = createBot({
         username,
-        host: "127.0.0.1",
+        host: "172.17.0.1",
         port: 25565,
         version: "1.12.2",
         checkTimeoutInterval: 5 * 60 * 1000
