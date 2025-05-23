d5b5f601eb5f17f5967b5483bec0d8df9c72387f
Raraph84 <contact@raraph.fr>
2025-05-23 15:47:21 +0200

    Add Node base

---
+++ node/src/Containers.js
@@ -0,0 +1,254 @@
+const { promises: fs, existsSync } = require("fs");
+const EventEmitter = require("events");
+const path = require("path");
+const config = require("../config.json");
+
+class Container extends EventEmitter {
+    /**
+     * @param {import("./Servers")} servers
+     * @param {string} name
+     * @param {string} path
+     * @param {number} port
+     */
+    constructor(servers, name, path, port) {
+        super();
+
+        this.servers = servers;
+        this.name = name;
+        this.path = path;
+        this.port = port;
+
+        /** @type {"stopped"|"starting"|"started"|"stopping"} */
+        this.state = "stopped";
+
+        this._bindDockerEventHandler = this._dockerEventHandler.bind(this);
+    }
+
+    /**
+     * @param {"stopped"|"starting"|"started"|"stopping"} state
+     */
+    _setState(state) {
+        this.state = state;
+        this.emit(state);
+        this.servers.gateway.clients
+            .filter((client) => client.infos.logged)
+            .forEach((client) => client.emitEvent("SERVER_STATE", { name: this.name, state: state }));
+    }
+
+    /**
+     * @param {object} event
+     */
+    async _dockerEventHandler(event) {
+        if (event.Type !== "container" || event.Actor.Attributes.name !== this.name) return;
+
+        if (event.Action === "start") this._postStart();
+        else if (event.Action === "die") this._postStop();
+    }
+
+    async _preStart() {
+        if (this.state !== "stopped") throw new Error("Not stopped");
+        this._setState("starting");
+
+        console.log("Starting container " + this.name + "...");
+
+        while (true) {
+            try {
+                await this.servers.docker.getContainer(this.name).remove({ force: true });
+            } catch (error) {
+                if (error.statusCode === 404) break;
+                else if (error.statusCode === 409) continue;
+                else throw error;
+            }
+        }
+    }
+
+    /**
+     * @param {number} memory
+     * @param {string} file
+     * @param {boolean} exposed
+     */
+    async start(memory, file, exposed = false) {
+        await this._preStart();
+
+        this.servers.dockerEvents.on("rawEvent", this._bindDockerEventHandler);
+
+        const container = await this.servers.docker.createContainer({
+            name: this.name,
+            ExposedPorts: { "25565/tcp": {} },
+            Tty: true,
+            OpenStdin: true,
+            Cmd: ["java", "-Xmx" + memory * 1024 + "M", "-jar", file],
+            Image: "openjdk:8",
+            WorkingDir: "/home/server",
+            HostConfig: {
+                PortBindings: {
+                    "25565/tcp": [{ HostIp: exposed ? "" : "172.17.0.1", HostPort: this.port.toString() }]
+                },
+                Binds: [this.path + ":/home/server"],
+                AutoRemove: true
+            }
+        });
+
+        await container.start();
+    }
+
+    _postStart() {
+        if (this.state !== "starting") throw new Error("Not starting");
+
+        this._setState("started");
+        console.log("Container " + this.name + " started.");
+    }
+
+    /**
+     * @param {string} command
+     */
+    async stop(command) {
+        if (this.state !== "started") throw new Error("Not started");
+        this._setState("stopping");
+
+        console.log("Stopping container " + this.name + "...");
+
+        await this._send(command);
+
+        const killTimeout = setTimeout(() => this.kill(), 30 * 1000);
+
+        await new Promise((resolve) =>
+            this.once("stopped", () => {
+                clearTimeout(killTimeout);
+                resolve();
+            })
+        );
+    }
+
+    _postStop() {
+        if (this.state === "stopped") throw new Error("Already stopped");
+
+        this.servers.dockerEvents.removeListener("rawEvent", this._bindDockerEventHandler);
+
+        this._setState("stopped");
+        console.log("Container " + this.name + " stopped.");
+    }
+
+    /**
+     * @param {string} content
+     */
+    async _send(content) {
+        const container = this.servers.docker.getContainer(this.name);
+        const stream = await container.attach({ stream: true, stdin: true });
+
+        await new Promise(async (resolve, reject) => {
+            stream.write(content + "\n", (error) => {
+                if (error) reject(error);
+                else
+                    stream.end((error) => {
+                        if (error) reject(error);
+                        else resolve();
+                    });
+            });
+        });
+    }
+
+    async kill() {
+        if (this.state === "stopped") throw new Error("Already stopped");
+
+        await this.servers.docker.getContainer(this.name).kill();
+    }
+}
+
+class Server extends Container {
+    /**
+     * @param {import("./Servers")} servers
+     * @param {string} name
+     */
+    constructor(servers, name) {
+        let port = config.serversStartingPort;
+        while (servers.servers.some((server) => server.port === port)) port++;
+
+        super(servers, name, path.join(path.resolve(config.serversDir), name), port);
+    }
+
+    async _preStart() {
+        await super._preStart();
+
+        if (existsSync(this.path)) await fs.rm(this.path, { recursive: true });
+    }
+
+    /**
+     * @param {number} memory
+     */
+    async start(memory) {
+        await super.start(memory, "spigot-1.12.2.jar");
+    }
+
+    async stop() {
+        await super.stop("stop");
+    }
+}
+
+class Lobby extends Server {
+    /**
+     * @param {import("./Servers")} servers
+     * @param {number} id
+     */
+    constructor(servers, id) {
+        super(servers, "lobby" + id);
+
+        this.id = id;
+    }
+
+    async _preStart() {
+        await super._preStart();
+
+        await fs.cp(path.resolve(config.lobbyServerTemplateDir), this.path, { recursive: true });
+
+        const configFile = path.join(
+            this.path,
+            "plugins",
+            "Infinite-Minecraft-Players-POC-Server-Plugin",
+            "config.yml"
+        );
+        await fs.writeFile(configFile, (await fs.readFile(configFile, "utf8")).replace("SERVERNAME", this.name));
+    }
+
+    async start() {
+        await super.start(config.lobbyServerMemory);
+    }
+}
+
+class Game extends Server {
+    /**
+     * @param {import("./Servers")} servers
+     * @param {number} id
+     */
+    constructor(servers, id) {
+        super(servers, "game" + id);
+
+        this.id = id;
+        this.gameStarted = false;
+    }
+
+    async _preStart() {
+        await super._preStart();
+
+        await fs.cp(path.resolve(config.gameServerTemplateDir), this.path, { recursive: true });
+
+        const configFile = path.join(
+            this.path,
+            "plugins",
+            "Infinite-Minecraft-Players-POC-Server-Plugin",
+            "config.yml"
+        );
+        await fs.writeFile(configFile, (await fs.readFile(configFile, "utf8")).replace("SERVERNAME", this.name));
+    }
+
+    async start() {
+        await super.start(config.gameServerMemory);
+    }
+
+    async _postStop() {
+        this.gameStarted = false;
+        await super._postStop();
+    }
+}
+
+module.exports = { Container, Proxy, Server, Lobby, Game };
